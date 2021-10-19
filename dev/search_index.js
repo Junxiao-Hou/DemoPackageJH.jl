034523237027188cfe01ecc95669dbf131b9e5a0
Documenter.jl <documenter@juliadocs.github.io>
2021-10-19 03:53:21 +0000

    build based on feffae2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageJH","category":"page"},{"location":"#DemoPackageJH","page":"Home","title":"DemoPackageJH","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageJH.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageJH]","category":"page"}]
+}
